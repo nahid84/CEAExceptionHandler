c7cef194a7c6d2ad9df9922bbc8fe5c856bdb7ff
Nahid Hasan <nahid.hasan@capgemini.com>
2020-08-17 11:11:42 +0200

    Version controller added

---
+++ CustomExceptionHanding/ExceptionDealer/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionDealer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private const string _version = "1.0.0";
+
+        public IActionResult GetVersion()
+        {
+            return Ok(@"{ ""Version"" : """ + _version + @""" }");
+        }
+    }
+}
